98f771a0a270adced0c6c22ce7c70e18a3814c99
Laurits Dyrberg Hansen <201706994@post.au.dk>
2019-03-22 11:33:00 +0100

    Changed formatting of output. Started tests for ATM

---
+++ SWT1ATM.Unit.Test/TestAtm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using SWT1ATM.Factory;
+
+namespace SWT1ATM.Unit.Test
+{
+    [TestFixture]
+    class TestAtm
+    {
+        
+     private Atm _uut;
+
+            [SetUp]
+            public void Setup()
+            {
+                _uut = new Atm(Substitute.For<ITrackFilter>());
+            }
+
+     
+        
+    }
+}
